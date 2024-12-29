e10da5782d42273895eed7b3a1cba0708ac853de
thushan-123 <thushanmadusanka456@gmail.com>
2024-12-29 15:45:13 +0530

    create socket

--- app/main.py
+++ app/main.py
@@ -1,1 +1,23 @@
-print("hello world")
+import socket
+import signal
+import threading
+
+def main():
+    PORT = 8080
+    
+    
+    server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    server.bind(("127.0.0.1",PORT))
+    server.listen()
+    
+    print(f"Proxy SERVER Listening on Port {PORT}")
+    
+    while True:
+        client_socket, addr = server.accept()
+        
+        print(f"Client Accept Connection From {addr[0]} - {addr[1]}")
+        
+        
+    
+if __name__ == "__main__":
+    main()
