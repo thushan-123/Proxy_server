4dc68dfe502e2dd05faf75120c7a41cea47a1580
thushan-123 <thushanmadusanka456@gmail.com>
2025-08-27 22:55:37 +0530

    update changes main

--- app/main.py
+++ app/main.py
@@ -2,15 +2,18 @@ import asyncio
 import struct
 import socket
 
-def handle_client():
-    pass
+from .handle_client import handle_user
 
 
 async def start_proxy(host="127.0.0.1", port=1080):
-    server = await asyncio.start_server(handle_client,host, port)
+    server = await asyncio.start_server(handle_user,host, port)
     address = server.sockets[0].getsockname()
-    print(f"SOCKS 5 proxy server running on {host} port {port}")
+    
+    print(f"SOCKS 5 proxy server running on {address[0]} port {address[1]}")
     
     async with server:
         await server.serve_forever()
+        
+        
+
 
